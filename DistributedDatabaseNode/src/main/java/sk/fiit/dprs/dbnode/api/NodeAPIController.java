2da519364e31c1d38544855a276574ebc96ba504
Jozef Zatko <jozefzatko.sk@gmail.com>
2016-05-16 01:56:06 +0200

    Pridana fasada API funkcii pre pracu nad uzlami

--- DistributedDatabaseNode/src/main/java/sk/fiit/dprs/dbnode/api/NodeAPIController.java
+++ DistributedDatabaseNode/src/main/java/sk/fiit/dprs/dbnode/api/NodeAPIController.java
@@ -1,5 +1,9 @@
 package sk.fiit.dprs.dbnode.api;
 
+import static spark.Spark.*;
+
+import org.apache.log4j.Logger;
+
 /**
  * Node-to-Node API setup and controlling
  * 
@@ -7,7 +11,66 @@ package sk.fiit.dprs.dbnode.api;
  */
 public class NodeAPIController {
 
+	static Logger log = Logger.getLogger(UserAPIController.class.getName());
+	
 	public NodeAPIController(String id, String consulURL) {
-
+		
+		String logMessage ="[ ]";
+		
+		/* 
+		 * READ data from DB node
+		 * 
+		 * GET http://localhost:4567/dbnode/1
+		 * GET http://localhost:4567/dbnode/1?from=12345&to=54321
+		 */
+		get("/dbnode/:replica", (request, response) -> {
+			
+			String requestID = request.headers("X-Request-Id");
+			log.info("requestID: "+requestID+" method: "+request.requestMethod() + " " + request.url() + " " + logMessage);
+			
+			return NodeAPIRequestProcessing.getDbNodeData(request.params(":replica"), request.queryParams("from"), request.queryParams("to"));
+		});
+		
+		/* 
+		 * WRITE data to DB node
+		 * 
+		 * POST http://localhost:4567/dbnode/1
+		 * POST http://localhost:4567/dbnode/1?from=12345&to=54321
+		 */
+		post("/dbnode/:replica", (request, response) -> {
+			
+			String requestID = request.headers("X-Request-Id");
+			log.info("requestID: "+requestID+" method: "+request.requestMethod() + " " + request.url() + " " + logMessage);
+			
+			return NodeAPIRequestProcessing.postDbNodeData(request.params(":replica"), request.queryParams("from"), request.queryParams("to"));
+		});
+		
+		/* 
+		 * PUT data to DB node
+		 * 
+		 * PUT http://localhost:4567/dbnode/1
+		 * PUT http://localhost:4567/dbnode/1?from=12345&to=54321
+		 */
+		put("/dbnode/:replica", (request, response) -> {
+			
+			String requestID = request.headers("X-Request-Id");
+			log.info("requestID: "+requestID+" method: "+request.requestMethod() + " " + request.url() + " " + logMessage);
+			
+			return NodeAPIRequestProcessing.putDbNodeData(request.params(":replica"), request.queryParams("from"), request.queryParams("to"));
+		});
+		
+		/* 
+		 * DELETE data from DB node
+		 * 
+		 * DELETE http://localhost:4567/dbnode/1
+		 * DELETE http://localhost:4567/dbnode/1?from=12345&to=54321
+		 */
+		delete("/dbnode/:replica", (request, response) -> {
+			
+			String requestID = request.headers("X-Request-Id");
+			log.info("requestID: "+requestID+" method: "+request.requestMethod() + " " + request.url() + " " + logMessage);
+			
+			return NodeAPIRequestProcessing.deleteDbNodeData(request.params(":replica"), request.queryParams("from"), request.queryParams("to"));
+		});
 	}
 }
---
+++ DistributedDatabaseNode/src/main/java/sk/fiit/dprs/dbnode/api/NodeAPIRequestProcessing.java
@@ -0,0 +1,26 @@
+package sk.fiit.dprs.dbnode.api;
+
+public class NodeAPIRequestProcessing {
+
+	
+	public static String getDbNodeData(String replica, String hashFrom, String hashTo) {
+		
+		return null;
+	}
+	
+	public static String postDbNodeData(String replica, String hashFrom, String hashTo) {
+		
+		return null;
+	}
+
+	public static String putDbNodeData(String replica, String hashFrom, String hashTo) {
+		
+		return null;
+	}
+
+	public static String deleteDbNodeData(String replica, String hashFrom, String hashTo) {
+		
+		return null;
+	}
+
+}
