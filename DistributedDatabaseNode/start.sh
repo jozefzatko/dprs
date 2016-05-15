4987459eb9fbef64b56046246c778e156e6f2744
daniel <daniel@daniel-virtual-machine>
2016-05-16 00:29:23 +0200

    pridane automaticke nahodenie logovania, zistenie adresy logovacieho uzla

---
+++ DistributedDatabaseNode/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+JSON=`curl -XGET $CONSUL_URL/v1/catalog/service/elk-9200 |  jq '.[0].ServiceAddress'`
+
+RES="${JSON//\"/}"
+echo $RES
+ADR="http://$RES:9200/_template/filebeat?pretty"
+echo $ADR
+curl -XPUT $ADR -d@/usr/bin/filebeat.template.json
+
+/usr/bin/filebeat -c /usr/bin/filebeat.yml -v -e &
+
