b53368b08c34d446e1f9b847a692fc1c8b726cb0
daniel <daniel@daniel-virtual-machine>
2016-05-16 03:27:14 +0200

    upravene parametre

--- DistributedDatabaseNode/start.sh
+++ DistributedDatabaseNode/start.sh
@@ -9,4 +9,4 @@ echo $ADR
 curl -XPUT $ADR -d@/usr/bin/filebeat.template.json
 
 /usr/bin/filebeat -c /usr/bin/filebeat.yml -v -e &
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /workdir/target/dbnode-jar-with-dependencies.jar
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /workdir/target/dbnode-jar-with-dependencies.jar $CONSUL_URL $SUPPORT_IP
