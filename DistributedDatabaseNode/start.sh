7451f2cb8de9449e60b1e24785cd128730358556
daniel <daniel@daniel-virtual-machine>
2016-05-16 03:05:59 +0200

    commit

--- DistributedDatabaseNode/start.sh
+++ DistributedDatabaseNode/start.sh
@@ -8,5 +8,5 @@ ADR="http://$RES:9200/_template/filebeat?pretty"
 echo $ADR
 curl -XPUT $ADR -d@/usr/bin/filebeat.template.json
 
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /workdir/target/dbnode-jar-with-dependencies.jar &
-/usr/bin/filebeat -c /usr/bin/filebeat.yml -v -e 
+/usr/bin/filebeat -c /usr/bin/filebeat.yml -v -e &
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /workdir/target/dbnode-jar-with-dependencies.jar
